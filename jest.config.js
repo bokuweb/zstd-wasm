7ccb4c9142e6b4cd64250841e354b0e143a7b7d9
bokuweb <bokuweb12@gmail.com>
2021-06-02 23:38:30 +0900

    env: Setup actions

---
+++ jest.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  testMatch: ['**/test/**/?(*.)+(spec|test).+(ts|tsx|js)'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+};
