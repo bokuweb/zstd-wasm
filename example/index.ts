78cef870155356cf9d797406b9e9568802a4b67a
bokuweb <bokuweb12@gmail.com>
2021-07-15 19:53:52 +0900

    fix:

--- example/index.ts
+++ example/index.ts
@@ -1,12 +1,10 @@
-import { decompress } from '../lib/simple/decompress';
-import { compress } from '../lib/simple/compress';
-import { init } from '../lib/init';
+import { init, compress, decompress } from '../lib';
 
 const hello = 'KLUv/SQMYQAASGVsbG8genN0ZCEhN2g+CQ==';
 
 (async () => {
   const decompressTest = async () => {
-    const res = await decompress(Buffer.from(hello, 'base64'));
+    const res = decompress(Buffer.from(hello, 'base64'));
     const str = Buffer.from(res).toString();
     console.log(str);
     if (str !== 'Hello zstd!!') throw new Error('Failed to decompressTest by zstd.');
@@ -15,14 +13,14 @@ const hello = 'KLUv/SQMYQAASGVsbG8genN0ZCEhN2g+CQ==';
   };
 
   const compressAndDecompressTest = async () => {
-    const compressed = await compress(Buffer.from('Hello zstd!!'), 10);
-    const res = await decompress(compressed);
+    const compressed = compress(Buffer.from('Hello zstd!!'), 10);
+    const res = decompress(compressed);
     const str = Buffer.from(res).toString();
     if (str !== 'Hello zstd!!') throw new Error('Failed to compressAndDecompressTest by zstd.');
     console.log(`%c${str}`, 'color: lightgreen;');
     console.log('%cSucceeded to compressAndDecompressTest.', 'color: lightgreen;');
   };
-  await init();
+  await init('./zstd.wasm');
   await decompressTest();
   await compressAndDecompressTest();
 })();
