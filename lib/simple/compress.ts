519979e7776a472620ef1afad300bf9cb3022f09
bokuweb <bokuweb12@gmail.com>
2021-07-27 11:00:44 +0900

    refactor

--- lib/simple/compress.ts
+++ lib/simple/compress.ts
@@ -1,18 +1,18 @@
 import { Module } from '../init';
-import { isError, getErrorName } from '../errors';
+import { isError } from '../errors';
 
 const compressBound = (size: number): number => {
-  const bound = Module.cwrap('ZSTD_compressBound', 'number', ['number']);
+  const bound = Module['_ZSTD_compressBound'];
   return bound(size);
 };
 
 export const compress = (buf: ArrayBuffer, level: number) => {
   const bound = compressBound(buf.byteLength);
-  const malloc = Module.cwrap('malloc', 'number', ['number']);
+  const malloc = Module['_malloc'];
   const compressed = malloc(bound);
   const src = malloc(buf.byteLength);
   Module.HEAP8.set(buf, src);
-  const free = Module.cwrap('free', 'number');
+  const free = Module['_free'];
   try {
     /*
       @See https://zstd.docsforge.com/dev/api/ZSTD_compress/
@@ -22,10 +22,10 @@ export const compress = (buf: ArrayBuffer, level: number) => {
       @return : compressed size written into `dst` (<= `dstCapacity),
                 or an error code if it fails (which can be tested using ZSTD_isError()).
     */
-    const _compress = Module.cwrap('ZSTD_compress', 'number', ['number', 'number', 'number', 'number', 'number']);
+    const _compress = Module['_ZSTD_compress'];
     const sizeOrError = _compress(compressed, bound, src, buf.byteLength, level);
     if (isError(sizeOrError)) {
-      throw new Error(getErrorName(sizeOrError));
+      throw new Error(`Failed to compress with code ${sizeOrError}`);
     }
     // // Copy buffer
     // // Uint8Array.prototype.slice() return copied buffer.
--- lib/simple/decompress.ts
+++ lib/simple/decompress.ts
@@ -1,8 +1,8 @@
 import { Module } from '../init';
-import { isError, getErrorName } from '../errors';
+import { isError } from '../errors';
 
 const getFrameContentSize = (src: number, size: number): number => {
-  const getSize = Module.cwrap('ZSTD_getFrameContentSize', 'number', ['number', 'number']);
+  const getSize = Module['_ZSTD_getFrameContentSize'];
   return getSize(src, size);
 };
 
@@ -14,12 +14,12 @@ export const decompress = (
   buf: ArrayBuffer,
   opts: DecompressOption = { defaultHeapSize: 1024 * 1024 }, // Use 1MB on default if it is failed to get content size.
 ): ArrayBuffer => {
-  const malloc = Module.cwrap('malloc', 'number', ['number']);
+  const malloc = Module['_malloc'];
   const src = malloc(buf.byteLength);
   Module.HEAP8.set(buf, src);
   const contentSize = getFrameContentSize(src, buf.byteLength);
   const size = contentSize === -1 ? opts.defaultHeapSize : contentSize;
-  const free = Module.cwrap('free', 'number');
+  const free = Module['_free'];
   const heap = malloc(size);
   try {
     /*
@@ -29,10 +29,10 @@ export const decompress = (
       If user cannot imply a maximum upper bound, it's better to use streaming mode to decompress data.
       @return: the number of bytes decompressed into dst (<= dstCapacity), or an errorCode if it fails (which can be tested using ZSTD_isError()).
     */
-    const _decompress = Module.cwrap('ZSTD_decompress', 'number', ['number', 'number', 'number', 'number']);
+    const _decompress = Module['_ZSTD_decompress'];
     const sizeOrError = _decompress(heap, size, src, buf.byteLength);
     if (isError(sizeOrError)) {
-      throw new Error(getErrorName(sizeOrError));
+      throw new Error(`Failed to compress with code ${sizeOrError}`);
     }
     // Copy buffer
     // Uint8Array.prototype.slice() return copied buffer.
