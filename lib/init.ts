bbbcee847a8cb4491f4918198b1aa819a81c3a85
bokuweb <bokuweb12@gmail.com>
2021-07-28 01:22:32 +0900

    fix

--- lib/init.ts
+++ lib/init.ts
@@ -1,4 +1,10 @@
+const { readFileSync } = require('fs');
+const { resolve } = require('path');
+
 const Module = require('./wasm/zstd.js');
+
+const IS_NODE =
+  typeof process === 'object' && typeof process.versions === 'object' && typeof process.versions.node === 'string';
 
 const initialized = (() =>
   new Promise<void>((resolve) => {
@@ -10,6 +16,13 @@ export const waitInitialized = async () => {
 };
 
 export const init = async (path?: string) => {
+  if (IS_NODE) {
+    const buf = readFileSync(resolve(__dirname, './wasm/zstd.wasm'));
+    Module['init'](buf);
+  } else {
+    var url = require('./wasm/zstd.wasm');
+    Module['init'](path ?? url.default ?? url);
+  }
   Module['init'](path);
   await waitInitialized();
 };
