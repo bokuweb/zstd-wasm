fe4fa16c727943a1ae6d68963e90ee270d39a6ec
bokuweb <bokuweb12@gmail.com>
2021-07-28 10:08:48 +0900

    fix

--- examples/webpack4/index.js
+++ examples/webpack4/index.js
@@ -1,18 +1,7 @@
 import { init, compress, decompress } from '@bokuweb/zstd-wasm';
 
-const hello = 'KLUv/SQMYQAASGVsbG8genN0ZCEhN2g+CQ==';
-
 (async () => {
-  const decompressTest = async () => {
-    const res = decompress(Buffer.from(hello, 'base64'));
-    const str = Buffer.from(res).toString();
-    console.log(str);
-    if (str !== 'Hello zstd!!') throw new Error('Failed to decompressTest by zstd.');
-    console.log(`%c${str}`, 'color: lightgreen;');
-    console.log('%cSucceeded to decompressTest.', 'color: lightgreen;');
-  };
-
-  const compressAndDecompressTest = async () => {
+  const compressAndDecompressTest = () => {
     const compressed = compress(Buffer.from('Hello zstd!!'), 10);
     const res = decompress(compressed);
     const str = Buffer.from(res).toString();
@@ -21,8 +10,7 @@ const hello = 'KLUv/SQMYQAASGVsbG8genN0ZCEhN2g+CQ==';
     console.log('%cSucceeded to compressAndDecompressTest.', 'color: lightgreen;');
   };
   await init();
-  await decompressTest();
-  await compressAndDecompressTest();
+  compressAndDecompressTest();
   document.body.innerText = 'Succeeded';
 })().catch((e) => {
   document.body.innerText = 'Failed';
