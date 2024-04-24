da145f6a2509a46e64ad3b12f64577dd28f946de
Thomas Schaper <thomas@codegrade.com>
2024-04-24 14:55:39 +0200

    Stop using readFileSync (#130)
    This ensures we don't block the event loop while reading files, which should result in a overall system performance improvement.

--- test/compress_using_dict.test.ts
+++ test/compress_using_dict.test.ts
@@ -1,4 +1,4 @@
-import { readFileSync } from 'fs';
+import { readFile } from 'fs/promises';
 import { resolve } from 'path';
 
 import { decompressUsingDict, createDCtx } from '../lib/simple/decompress_using_dict';
@@ -7,8 +7,8 @@ import { init } from '../lib/index.node';
 
 test('using dict', async () => {
   await init();
-  const buf = readFileSync(resolve(__dirname, './mock/mock.json'));
-  const dict = readFileSync(resolve(__dirname, './json-dict'));
+  const buf = await readFile(resolve(__dirname, './mock/mock.json'));
+  const dict = await readFile(resolve(__dirname, './json-dict'));
   const compressed = compressUsingDict(createCCtx(), buf, dict, 10);
   const decompressed = decompressUsingDict(createDCtx(), compressed, dict);
   expect(buf.toString()).toBe(Buffer.from(decompressed).toString());
--- test/largefile.test.ts
+++ test/largefile.test.ts
@@ -1,4 +1,4 @@
-import { readFileSync } from 'fs';
+import { readFile } from 'fs/promises';
 import { resolve } from 'path';
 
 import { decompress } from '../lib/simple/decompress';
@@ -7,7 +7,7 @@ import { init } from '../lib/index.node';
 
 test('largefile', async () => {
   await init();
-  const buf = readFileSync(resolve(__dirname, './large-file'));
+  const buf = await readFile(resolve(__dirname, './large-file'));
   const compressed = compress(buf, 10);
   const decompressed = decompress(compressed);
   expect(buf.equals(Buffer.from(decompressed))).toBeTruthy();
