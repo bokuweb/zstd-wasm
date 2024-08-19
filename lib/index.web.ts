36116048acd6de52ba4dd0cb929f65d5088d4ef3
bokuweb <bokuweb12@gmail.com>
2024-08-19 17:59:17 +0900

    fix: errors

--- lib/index.web.ts
+++ lib/index.web.ts
@@ -1,6 +1,7 @@
 import { Module, waitInitialized } from './module';
 
 export const init = async (path?: string) => {
+  // @ts-ignore
   const url = new URL(`./zstd.wasm`, import.meta.url).href;
   Module['init'](path ?? url);
   await waitInitialized();
